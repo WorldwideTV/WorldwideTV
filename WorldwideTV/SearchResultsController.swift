a91b770695eef3bcbbc4f680145d0052244765e1
Rui Costa <rui.pfcosta@gmail.com>
2016-01-31 00:05:05 +0000

    Implemented search!

--- WorldwideTV/SearchResultsController.swift
+++ WorldwideTV/SearchResultsController.swift
@@ -1,20 +1,26 @@
 import UIKit
 
-class SearchResultsController: UIViewController, UISearchResultsUpdating {
+class SearchResultsController: ChannelsListViewController, UISearchResultsUpdating {
     
     override func loadView() {
         super.loadView()
         view.backgroundColor = UIColor.purpleColor()
     }
-    
+        
     // UISearchResultsUpdating
     
     func updateSearchResultsForSearchController(searchController: UISearchController) {
-        guard let searchQuery = searchController.searchBar.text where searchQuery.characters.count > 0 else {
+        guard let searchQuery = searchController.searchBar.text where searchQuery.characters.count > 0,
+              let allCountries = TVFetcherService.sharedInstance.countries else {
             return
         }
-
-        print("search query: \(searchQuery)")
+        
+        let allChannels = allCountries.flatMap { $0.channels }
+        let matchingCountries = allCountries.filter { $0.title.lowercaseString.containsString(searchQuery.lowercaseString) }
+        let matchingChannels = allChannels.filter { $0.title.lowercaseString.containsString(searchQuery.lowercaseString) }
+        
+        let everything = matchingCountries.flatMap{ $0.channels } + matchingChannels
+        
+        channels = Array(Set<WWChannel>(everything))
     }
-
 }
