532996b8fa206d0db94ed8c04198d880e7d43aa3
Rui Costa <rui.pfcosta@gmail.com>
2016-02-02 00:41:53 +0000

    Improved search.

--- WorldwideTV/SearchResultsController.swift
+++ WorldwideTV/SearchResultsController.swift
@@ -6,23 +6,25 @@ class SearchResultsController: ChannelsListViewController, UISearchResultsUpdati
         super.loadView()
         view.backgroundColor = UIColor.purpleColor()
     }
-        
+       
     // UISearchResultsUpdating
     
     func updateSearchResultsForSearchController(searchController: UISearchController) {
-        guard let searchQuery = searchController.searchBar.text where searchQuery.characters.count > 0,
-              let allCountries = TVFetcherService.sharedInstance.countries else {
+        guard let allCountries = TVFetcherService.sharedInstance.countries else {
             return
         }
         
         let allChannels = allCountries.flatMap { $0.channels }
-        let matchingCountries = allCountries.filter { $0.title.lowercaseString.containsString(searchQuery.lowercaseString) }
-        let matchingChannels = allChannels.filter { $0.title.lowercaseString.containsString(searchQuery.lowercaseString) }
+        let results: [WWChannel]
         
-        let everything = matchingCountries.flatMap{ $0.channels } + matchingChannels
-        
-        if everything.count > 0 {
-            channels = Array(Set<WWChannel>(everything))
+        if let searchQuery = searchController.searchBar.text where searchQuery.characters.count > 0 {
+            let matchingCountries = allCountries.filter { $0.title.lowercaseString.containsString(searchQuery.lowercaseString) }
+            let matchingChannels = allChannels.filter { $0.title.lowercaseString.containsString(searchQuery.lowercaseString) }
+            results = matchingCountries.flatMap{ $0.channels } + matchingChannels
+        } else {
+            results = allChannels
         }
+        
+        channels = Array(Set<WWChannel>(results))
     }
 }
